c8fb9ad634abc41724ce6025ba8c029eeb3597ce
thsumman <35498753+thsumman@users.noreply.github.com>
2018-01-28 14:10:30 +0200

    Harjoituksen 2.11 tiedostot, hyödyntävät myös tiedostoa Numerot_2_6__2_10.js tässä repositoryssa.

---
+++ Appi_2_11.js
@@ -0,0 +1,106 @@
+import React from 'react'
+import axios from 'axios'
+import Numerot from './Numerot_2_6__2_10'
+
+class App extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        persons: [
+        ],
+        newName: 'jotain',
+        newNumber: 'joku numero',
+        filter: '',
+        personsToShow: [
+          { name: 'Arto Hellas', number: '040-123456' },
+          { name: 'Martti Tienari', number: '040-123456' },
+          { name: 'Arto Järvinen', number: '040-123456' },
+          { name: 'Lea Kutvonen', number: '040-123456' }
+        ]
+      }
+    }
+  
+    addName = (event) => {
+      event.preventDefault()
+      const findSameName = (element) => {
+        return (
+          element.name === this.state.newName
+        )
+      }
+      const apu_ind = this.state.persons.findIndex(findSameName);
+  
+      if (apu_ind === -1) {
+        const personObject = {
+          name: this.state.newName,
+          number: this.state.newNumber
+        }
+        const persons = this.state.persons.concat(personObject)
+        const personsToShow = persons
+        this.setState({
+          persons,
+          newName: '',
+          newNumber: '',
+          filter: '',
+          personsToShow
+        })
+      }
+      else {
+        alert("Nimi on jo luettelossa")
+      }
+    }
+  
+    handleNameChange = (event) => {
+      console.log(event.target.value)
+      this.setState({ newName: event.target.value })
+    }
+  
+    handleNumberChange = (event) => {
+      console.log(event.target.value)
+      this.setState({ newNumber: event.target.value })
+    }
+  
+    handleChoose = (event) => {
+      const personsToShow = this.state.persons.filter((person) => person.name.toLowerCase().indexOf(event.target.value.toLowerCase(),0) === 0
+      )
+      this.setState({ filter: event.target.value })
+      this.setState({personsToShow: personsToShow})
+    }
+  
+    componentWillMount() {
+      console.log('will mount')
+      axios
+        .get("http://localhost:3001/persons")
+        .then(response => {
+          this.setState({persons: response.data})
+        }
+      )
+
+    }
+
+    render() {
+      return (
+        <div>
+          <h1>Puhelinluettelo</h1>
+          Rajaa näytettäviä: <input value={this.state.filter} onChange={this.handleChoose}/>
+          <h2>Lisää numero</h2>
+          <form onSubmit={this.addName}>
+            <div>
+              nimi: <input value={this.state.newName}  onChange={this.handleNameChange} />
+            </div>
+            <div>
+              numero: <input value={this.state.newNumber}  onChange={this.handleNumberChange}/>
+            </div>
+            <div>
+              <button type="submit">lisää</button>
+            </div>
+          </form>
+          <h2>Numerot</h2>
+          <div>
+            <Numerot persons={this.state.personsToShow}/>
+          </div>
+        </div>
+      )
+    }
+  }
+
+  export default App
